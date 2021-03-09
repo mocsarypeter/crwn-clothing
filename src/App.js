a14d15fb55df01dd31e4651bded36aa7d01011dd
mocsaryp1 <peter.mocsary@idomsoft.hu>
2021-03-09 19:38:18 +0100

    Sign in Page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
